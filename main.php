91aadfd1d5d07b63a720099e79d276dc049baf6c
zhenya <zhenyagrigorev@mail.ru>
2021-03-01 20:37:15 +0300

    Fixed code

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
 
-use app\Farm;
-use app\CreateAnimal\{CreateChicken,CreateCow};
+use app\CreateAnimal\CreateChicken;
+use app\CreateAnimal\CreateCow;
 
 
 require_once __DIR__ . '/vendor/autoload.php';
